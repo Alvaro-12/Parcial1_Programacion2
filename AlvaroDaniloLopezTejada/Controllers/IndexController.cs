3e81038ccf8707becb73b0d021342cec2747d1db
Alvaro Lopez <lt0895032020@unab.edu.sv>
2021-09-09 11:22:08 -0600

    vistas

---
+++ AlvaroDaniloLopezTejada/Controllers/IndexController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlvaroDaniloLopezTejada.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            if ()
+            {
+                return Aporbado();
+            }
+            else 
+            {
+
+                return Error();
+            }
+   
+        }
+        public ActionResult Aporbado()
+        {
+
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+
+
+            return View();
+        }
+    }
+
+}
